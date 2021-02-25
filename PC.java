2836b8b82b55995e1890039553d468e97523d9b7
hp <hp@LAPTOP-76NHQ9AJ>
2021-02-25 13:41:58 +0530

    commited

--- PC.java
+++ PC.java
@@ -3,7 +3,7 @@ public class Practiseclass {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("PC created");
-        System.out.println("a b c");
+        System.out.println("a1 b c");
 	}
 
 }
