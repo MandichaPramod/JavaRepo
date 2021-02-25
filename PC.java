f9cb0b476a61ed667140011f07bb9a1c2b4f4468
hp <hp@LAPTOP-76NHQ9AJ>
2021-02-25 13:47:55 +0530

    hi

--- PC.java
+++ PC.java
@@ -3,7 +3,7 @@ public class PC {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("PC created");
-        System.out.println("a1 b c");
+        System.out.println("a1 b2 c");
 	}
 
 }
